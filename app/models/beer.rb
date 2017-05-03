2a63154aa135fea07b8f312dcf1264835eb674a7
Livia_Santos <liviamelq@gmail.com>
2017-05-03 21:37:12 +1000

    Add validation to forms

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,9 @@ class Beer < ApplicationRecord
   has_many :comments
   acts_as_votable
 
+  validates :name, :beer_type, :unit_type, :price, :quantity, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :price, :quantity, numericality: true
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, uniqueness: true;
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
