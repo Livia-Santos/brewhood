bdedecdfc211d132246c11aa4a7203962f3fe338
Livia_Santos <liviamelq@gmail.com>
2017-05-09 19:32:56 +1000

    Setup Orders with stripe  on beers view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,56 +1,28 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :amount_to_be_charged
-  before_action :description
-
-  def new
-  end
-
 
   def create
-    @order = Order.new(params[:order])
+    @order = Order.new
+    @beer = Beer.find(params[:beer_id])
     @order.user_id = current_user.id
-    # user Id has many beers
-    # I have to know in wich beer I am to pass to the databe
-    # Struggling point Understand who can I pass the Beer_id(find)
-
+    @order.beer = @beer
 
     customer = StripeTool.create_customer(email: params[:stripeEmail],
                                           stripe_token: params[:stripeToken])
 
     charge = StripeTool.create_charge(customer_id: customer.id,
-                                      amount: @amount,
-                                      description: @description)
+                                      amount: @beer.price_in_cents,
+                                      description: @beer.name)
 
     # Stripe card is going to the db.
     @order.stripe_charge_id = charge.id
 
-    if @order.save
-      redirect_to thanks_path
-    else
-      render 'new'
-    end
+    @order.save!
+    redirect_to beer_path(@beer)
 
-    rescue Stripe::CardError => e
+
+  rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_order_path
+    redirect_to beer_path(@beer)
   end
-
-  def thanks
-  end
-
-  def order_params
-      params.require(:order).permit(:user_id, :beer_id, :quantity, :stripe_charge_id)
-  end
-
-  private
-
-  def description
-    @description = "Some amazing product"
-  end
-
-  def amount_to_be_charged
-    @amount = 1000
-  end
-
 end
