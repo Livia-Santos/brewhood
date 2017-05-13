15c9c551e82dfa6355d41486d8eb3f738ebd9b0d
Livia_Santos <liviamelq@gmail.com>
2017-05-13 12:51:36 +1000

    Setup mailgun for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtkp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'young-crag-54781.herokuapp.com',
+  :authentication => :plain,
+}
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
