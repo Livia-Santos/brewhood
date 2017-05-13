65b1ecd64df26e6143e76efc7d391726e6f3e8b4
Livia_Santos <liviamelq@gmail.com>
2017-05-13 10:37:30 +1000

    Setup avatar size

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ApplicationRecord
   validates :description, length: { minimum: 25 }
 
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def country
